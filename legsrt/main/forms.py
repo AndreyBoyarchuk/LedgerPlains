ace2432fb5282d7d8cfcb12351a05eadc8d55d4b
AndreyBoyarchuk <andreyboyarchuk@gmail.com>
2023-05-18 19:06:17 -0600

    restructure

--- legsrt/main/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Ledger
-
-class LedgerForm(forms.ModelForm):
-    class Meta:
-        model = Ledger
-        fields = ['transaction_date', 'description', 'amount']
--- legsrt/main/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Ledger(models.Model):
-   transaction_date = models.DateField()
-   description = models.TextField()
-   amount = models.DecimalField(max_digits=10, decimal_places=2)
